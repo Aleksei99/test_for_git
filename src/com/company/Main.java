d2690cb63fb3218ab9f5f7e327e0769eb0456b1c
Yakimchuk Viktar <yakimchukviktar@gmail.com>
2022-04-25 22:43:24 +0300

    add_passwords_generator

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -13,5 +15,29 @@ public class Main {
 
         System.out.println("some more");
         System.out.println("some");
+        byte byt = 15;
+        String str = "add new String";
+
+        System.out.println("Вас приветствует генератор паролей!");
+        System.out.print("Укажите длину пароля (количество символов)? ");
+        int lengthPassword = getUserNumber();
+        while (true) {
+            if (lengthPassword < 5) {
+                lengthPassword = getUserNumber();
+            } else break;
+        }
+        System.out.println("Ваш пароль: ");
+        for (int i = 0; i < lengthPassword; i++) {
+            System.out.print((char) (short) (Math.random() * 94 + 33));
+        }
+    }
+
+    private static int getUserNumber() {
+        System.out.print("Введите целое число не менее 5: ");
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.hasNextInt()) {
+            return scanner.nextInt();
+        } else System.out.print("Вы ввели не целое число. ");
+        return getUserNumber();
     }
 }
