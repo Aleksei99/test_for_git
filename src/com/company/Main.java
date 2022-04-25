8793a74c2a1fbf8db31aa74edc168cfb4cf60c8a
Aleksei <smurahaleha@gmail.com>
2022-04-25 22:15:05 +0300

    added new

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, this is my logic");
         System.out.println("Привет из тестового задания");
-
+        System.out.println("new");
     }
 }
