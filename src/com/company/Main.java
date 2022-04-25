0029ea15d91e13d276f323a93c72723b2575ba33
Yakimchuk Viktar <yakimchukviktar@gmail.com>
2022-04-25 22:02:49 +0300

    add_variables

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, this is my logic");
         System.out.println("Привет из тестового задания");
+        int b = 15;
+        String s = "непустая строка";
 
     }
 }
