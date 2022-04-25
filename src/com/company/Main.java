866e8195d406ed099e9ed94c184797549376f0de
Yakimchuk Viktar <yakimchukviktar@gmail.com>
2022-04-25 21:24:17 +0300

    last

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, this is my logic");
+        System.out.println("Привет из тестового задания");
+
     }
 }
